051b7c901d91c105e8bbe40e198505a1d44ff14b
Gregory Dukes <kcils360@gmail.com>
2017-09-26 10:29:43 -0700

    basic requirements met

---
+++ lab11Gregory/lab11Gregory/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab11Gregory.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Welcome to Codefellows Student Portal. \n \n" +
+                   "To register a new student, go to url and enter /Register \n \n" +
+                   "For a list of Students, go to url and enter /Students/Enrolled \n \n" +
+                   "For a list of Instructors, got to url and enter /Students/Faculty";
+        }
+        public string Register(string name, int grade)
+        {
+
+            return ("\n \n To register, in URL type ?name=<name of student>&grade=<number of entering grade>\n \n" +
+                    $"Student Name: {name}\n" +
+                    $"Grade Regeistering for: {grade}");
+        }
+    }
+
+}
+
---
+++ lab11Gregory/lab11Gregory/Controllers/StudentsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab11Gregory.Controllers
+{
+    public class StudentsController : Controller
+    {
+        public string Enrolled()
+        {
+
+            return ("Here is a list of all the students enrolled at Codefellows. \n \n" +
+                    "To return to Home page, go to url and type /Home");
+        }
+
+        public string Faculty()
+        {
+            return ("Here is a list of the Faculty at Codefellows \n" +
+                    "This list is currently empty, please check back later.  \n \n" +
+                    "To retun to Home page, got to url and type /Home");
+        }
+    }
+}
